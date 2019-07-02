a8b8f8c0f5b2dd49081851ed00a7f07c2f0ced37
utkarsh6799 <utkarshsingh6799@gmail.com>
2019-07-02 10:06:27 +0530

    json parsing and customizing http request

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/5010c46ff29777bca6021770f264e423/37.8267,-122.4233'
+const url = 'https://api.darksky.net/forecast/5010c46ff29777bca6021770f264e423/37.8267,-122.4233?lang=en'
 
-request({url: url}, (error,response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
+request({url: url, json:true}, (error,response) => {
+    
+    console.log(response.body.daily.data[0].summary + ' ' + 'it is currently' +' '+ response.body.currently.temperature + ' ' + 'degrees out')
+    
 })
