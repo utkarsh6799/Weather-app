70342313e2034b2032bf1110676c774ddbfb6504
utkarsh6799 <utkarshsingh6799@gmail.com>
2019-07-02 15:21:14 +0530

    input location from user

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
+const fs = require('fs')
 
+const address = process.argv[2]
 
-
-
-geocode('lucknow',(error,data) => {
-
-      if(error){
-            return console.log(error)
-      }
-
-      forecast(data.latitude, data.longitude, (error, forecastData) => {  //Callback Chaining
+if(!address){
+      console.log('Please provide an address!')
+}else{
+      geocode(address,(error,data) => {
 
             if(error){
                   return console.log(error)
             }
-
-             console.log(data.location)
-             console.log(forecastData)
-          })
-})
+      
+            forecast(data.latitude, data.longitude, (error, forecastData) => {  //Callback Chaining
+      
+                  if(error){
+                        return console.log(error)
+                  }
+      
+                   console.log(data.location)
+                   console.log(forecastData)
+                })
+      })
+}
