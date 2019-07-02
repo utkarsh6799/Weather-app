9c045fc8c4d7ed30c41f749916aad8dc75e70c5a
utkarsh6799 <utkarshsingh6799@gmail.com>
2019-07-02 11:43:15 +0530

    Error handling in geocoding

--- app.js
+++ app.js
@@ -14,11 +14,22 @@ request({url: url, json:true}, (error,response) => {
     
 })
 
+//Geocoding
+
 const geocadeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoidXRrYXJzaC1zaW5naDY3OTkiLCJhIjoiY2p4bGJ0OW13MDQ2MzN0bnQ3dng4OXl6MSJ9.i9J1ZIUkW9HDdw33wkvpJg&limit=1'
 
 request({url : geocadeUrl , json : true }, (error,response) => {
-    const latitude = response.body.features[0].center[1]
-    const longitude = response.body.features[0].center[0]
-    console.log(latitude,longitude)
+
+    if(error){
+        console.log('Unable to connect to the Internet!')
+    }else if(response.body.features.length === 0){
+        console.log('Location not found!')
+    }
+    else{
+        const latitude = response.body.features[0].center[1]
+        const longitude = response.body.features[0].center[0]
+        console.log(latitude,longitude)
+    }
+    
 
 })
