39ebf0b70b0b5be7aa7d8d72e322ae02dd9e65c0
utkarsh6799 <utkarshsingh6799@gmail.com>
2019-07-02 15:06:49 +0530

    error handling in forecast and geocode

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ const forecast = require('./utils/forecast')
 
 
 geocode('lucknow',(error,data) => {
-      console.log('Error', error)
-      console.log('Data' , data)
+
+      if(error){
+            return console.log(error)
+      }
 
       forecast(data.latitude, data.longitude, (error, data) => {  //Callback Chaining
+
+            if(error){
+                  return console.log(error)
+            }
             console.log('Error', error)
             console.log('Data', data)
           })
