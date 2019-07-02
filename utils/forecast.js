f793ec1f8b08c1af06126b51365ede53e7a659d5
utkarsh6799 <utkarshsingh6799@gmail.com>
2019-07-02 21:39:11 +0530

    es6 destructuring

--- utils/forecast.js
+++ utils/forecast.js
@@ -2,15 +2,15 @@ const request = require('request')
 
 
 const forecast = (latitude,longitude,callback) => {
-    const foreUrl = 'https://api.darksky.net/forecast/5010c46ff29777bca6021770f264e423/' + latitude + ',' + longitude + '?lang=en'
+    const url = 'https://api.darksky.net/forecast/5010c46ff29777bca6021770f264e423/' + latitude + ',' + longitude + '?lang=en'
 
-    request({ url : foreUrl , json : true}, (error,response) => {
+    request({ url , json : true}, (error,{ body }) => {
     if(error){
           callback('Unable to connect to internet', undefined)
-    }else if(response.body.error){
+    }else if(body.error){
           callback('Unable to find the location', undefined)
     }else{
-          callback(undefined, response.body.daily.data[0].summary + ' ' + 'it is currently' +' '+ response.body.currently.temperature + ' ' + 'degrees out')
+          callback(undefined, body.daily.data[0].summary + ' ' + 'it is currently' +' '+ body.currently.temperature + ' ' + 'degrees out')
     }
     })
 }
