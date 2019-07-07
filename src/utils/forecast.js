8150073962067c2638767a30b47fb6d8d6526f32
utkarsh6799 <utkarshsingh6799@gmail.com>
2019-07-07 13:46:59 +0530

    User interface added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,9 +10,10 @@ const forecast = (latitude,longitude,callback) => {
     }else if(body.error){
           callback('Unable to find the location', undefined)
     }else{
-          callback(undefined, body.daily.data[0].summary + ' ' + 'it is currently' +' '+ body.currently.temperature + ' ' + 'degrees out')
+          callback(undefined,body.daily.data[0].summary + ' ' + 'It is currently' +' '+ body.currently.temperature + ' ' + 'Fahrenheit out with' + ' ' + (body.daily.data[0].humidity)*100 + ' ' + '% humidity')
     }
     })
 }
 
 module.exports = forecast
+
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -12,7 +12,7 @@ const geocode = (address,callback) => {
         }else{
             callback(undefined, {
                 latitude: body.features[0].center[1],
-                longitude: body.features[0].center[0],
+                longitude: body.features[0].center[0], 
                 location : body.features[0].place_name
             })
         }
